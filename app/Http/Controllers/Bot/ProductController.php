1a9ef9e03002f4092f9f86c32cbdc3bacfbc3266
sonnet <ssonnet36@gmail.com>
2020-11-18 19:37:04 +0600

    launced v.1.2

--- app/Http/Controllers/Bot/ProductController.php
+++ app/Http/Controllers/Bot/ProductController.php
@@ -50,11 +50,15 @@ class ProductController extends Controller
             ->with('images')
             ->with('discounts')->paginate(10);*/
         if (request()->cat_id == 0) {
-            $products = Product::where('shop_id', $shop->id)->where('state', 1)
+            $products = Product::where('shop_id', $shop->id)
+                ->where('state', 1)
+                ->where('show_in_bot', 1)
                 ->with('images')
                 ->with('discounts')->paginate(2);
         } else {
-            $products = Product::where('category_id', request()->cat_id)->where('shop_id', $shop->id)->where('state', 1)
+            $products = Product::where('category_id', request()->cat_id)
+                ->where('shop_id', $shop->id)
+                ->where('state', 1)
                 ->with('images')
                 ->with('discounts')->paginate(2);
         }
